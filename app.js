828c6ffd658e6ca7ea775b8fd755edcf16546a30
melvinSosaMedrano <115840261+melvinSosaMedrano@users.noreply.github.com>
2022-10-23 15:38:35 -0600

    Update app.js

--- app.js
+++ app.js
@@ -4,5 +4,4 @@ const Server = require('./models/server');
 
 const server = new Server();
 
-server.listen(process.env.PORT || 3001, '0.0.0.0', () => {
-  console.log("Server is running.");
+server.listen();
